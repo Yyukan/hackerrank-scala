e2bac22c4f94617889a4c9ba5d83dbd735ced5ce
Oleksandr Shtykhno <oshtykhno@bol.com>
2017-08-23 11:23:33 +0200

    Simple for-loop

--- src/main/scala/net/shtykhno/algorithms/warmup/VeryBigSum.scala
+++ src/main/scala/net/shtykhno/algorithms/warmup/VeryBigSum.scala
@@ -3,18 +3,25 @@ package net.shtykhno.algorithms.warmup
 object VeryBigSum {
 
   def aVeryBigSum(n: Int, ar: Array[Long]): Long =  {
-    ar.sum
+    var sum: Long = 0L
+
+    for (i <- 0 until n) {
+      sum += ar(i)
+    }
+
+    sum
   }
 
   def main(args: Array[String]) {
     val sc = new java.util.Scanner (System.in)
-    var n = sc.nextInt()
-    var ar = new Array[Long](n)
+    val n = sc.nextInt()
+    val ar = new Array[Long](n)
+
     for(ar_i <- 0 until n) {
       ar(ar_i) = sc.nextLong()
     }
-    val result = aVeryBigSum(n, ar)
-    println(result)
+
+    println(aVeryBigSum(n, ar))
   }
 
 }
