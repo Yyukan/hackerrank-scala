2023d8ee5cbc8e9fd2bfea38e4d7c4a34df1342e
Oleksandr Shtykhno <yukan@ukr.net>
2017-08-26 12:39:37 +0200

    Add solution for ransom note

--- src/main/scala/net/shtykhno/algorithms/crackingandcoding/RansomNote.scala
+++ src/main/scala/net/shtykhno/algorithms/crackingandcoding/RansomNote.scala
@@ -1,8 +1,44 @@
 package net.shtykhno.algorithms.crackingandcoding
 
 /**
-  * Created 26/08/2017.
-  */
+ * Given the words in the magazine and the words in the ransom note,
+ * print Yes if he can replicate his ransom note exactly using whole words from the magazine;
+ * otherwise, print No.
+ */
 object RansomNote {
 
+  def solution(src: Array[String], dest: Array[String]): String = {
+
+    val srcWords: Map[String, Int] = src
+      .groupBy(identity)
+      .mapValues(_.length)
+
+    val destWords: Map[String, Int] = dest
+      .groupBy(identity)
+      .mapValues(_.length)
+
+    for ( (key, value) <- destWords) {
+      if (!srcWords.contains(key) || value > srcWords(key)) {
+        return "No"
+      }
+    }
+
+    "Yes"
+  }
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    var m = sc.nextInt()
+    var n = sc.nextInt()
+    var magazine = new Array[String](m)
+    for(magazine_i <- 0 until m) {
+      magazine(magazine_i) = sc.next()
+    }
+    var ransom = new Array[String](n)
+    for(ransom_i <- 0 until n) {
+      ransom(ransom_i) = sc.next()
+    }
+
+    println(solution(magazine, ransom))
+  }
 }
